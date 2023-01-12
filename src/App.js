6ab69097e9a18c599a844d79849f847310e134bd
rohankishorneog <rkn2601@gmail.com>
2023-01-12 19:39:52 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [name, setName]=useState("");
+  const [age, setAge]=useState(0);
+  const [height, setHeight]=useState(0);
+  const [superPower, setSuperpower]=useState("");
+  const [display, setDisplay]=useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+      <h1>Build A Hero</h1>
+      <label>Name</label>
+      <input type="text" style={{margin : 10}} onChange={(event)=>{setName(event.target.value)}}/>
+      <label>Age</label>
+      <input type="Number" style={{margin : 10}} onChange={(event)=>{setAge(event.target.value)}}/>
+      <label>Height</label>
+      <input type="Number" style={{margin : 10}}  onChange={(event)=>{setHeight(event.target.value)}}/>
+      <label>SuperPower</label>
+      <input type="text" style={{margin : 10}} onChange={(event)=>{setSuperpower(event.target.value)}}/>
+      </div>
+      <div>
+        <button onClick={()=>setDisplay(true)}>Display Charecter</button>
+      </div>
+      <div><h1>Hero Info</h1></div>
+      {display &&(
+        <ul>
+          <li>Name:{name}</li>
+          <li>Age: {age}</li>
+          <li>height: {height}</li>
+          <li>SuperPower: {superPower}</li>
+        </ul>
+      )}
     </div>
   );
 }
